f645f4f4e2064a6fe48cae634d6cfd082bd3ab97
Shahriar Ahmod <ashahriyar71@gmail.com>
2022-05-03 01:14:55 +0600

    1st

---
+++ src/components/List.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const List = ({iteams, onclick}: {
+    iteams: string[],
+    onclick: () => void
+}) => {
+  return (
+    <div>List</div>
+  )
+}
+
+export default List
