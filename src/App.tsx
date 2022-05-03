dc2f45b13c4d7d733d3d6bc9120136bb16371142
Shahriar Ahmod <ashahriyar71@gmail.com>
2022-05-03 11:40:15 +0600

    4th

--- src/App.tsx
+++ src/App.tsx
@@ -1,37 +1,89 @@
-import React, { useReducer } from 'react';
-import logo from './logo.svg';
-import './App.css';
-// import Lists from './components/Lists';
+import React, { useCallback, useReducer, useRef, useState } from "react";
+import "./App.css";
 
-// const Box: React.FunctionComponent<{title: string}> = ({title}) => {
+// const Box: React.FunctionComponent<{ title: string }> = ({ title }) => {
 //   return <div></div>;
 // };
-interface todo {
+
+interface Todo {
   id: number;
   text: string;
 }
-type ActionType = {tyoe: "ADD"; text: string} | {type: "REMOVE"; id: string};
+// Todo[]
+type ActionType =
+  | { type: "ADD"; text: string }
+  | { type: "REMOVE"; id: number };
 
 function App() {
-  function reducer(state:Todo[], action: ActionType) {
-    switch(action.type) {
+  const [myState, setMyState] = useState<Todo>();
+
+  // useEffect(() => {
+
+  // }, [])
+
+  // function reducer(state: Todo[], action: ActionType) {
+  //   switch (action.type) {
+  //     case "ADD":
+  //       return [
+  //         ...state,
+  //         {
+  //           id: state.length,
+  //           text: action.text,
+  //         },
+  //       ];
+  //     case "REMOVE":
+  //       return state.filter(({ id }) => id !== action.id);
+  //   }
+  // }
+  const [todos, dispatch] = useReducer((state: Todo[], action: ActionType) => {
+    switch (action.type) {
       case "ADD":
         return [
           ...state,
           {
             id: state.length,
-          }
-        ]
-  }
-}
-  const [todos, dispatch] = useReducer(reducer, []);
+            text: action.text,
+          },
+        ];
+      case "REMOVE":
+        return state.filter(({ id }) => id !== action.id);
+    }
+  }, []);
+
+  // [{}, {}, {}]
+
+  const newTodoRef = useRef<HTMLInputElement>(null);
+  // {} === {} // false
+  // useCallback
+  const onAddTodo = useCallback(() => {
+    if (newTodoRef.current) {
+      dispatch({
+        type: "ADD",
+        text: newTodoRef.current.value,
+      });
+      newTodoRef.current.value = "";
+    }
+  }, []);
+
   return (
-  <div className="App">
-    {/* <Box title="hi"></Box>
-    <Lists /> */}
+    // useReducer
+
+    <div className="App">
+      {/* <Box title="hello">awekfjalwekj</Box> */}
+      {/* <Lists /> */}
+
+      <input type="text" ref={newTodoRef} />
+      <button onClick={onAddTodo}>Add</button>
+      {todos.map((todo) => (
+        <div key={todo.id}>
+          {todo.text}
+          <button onClick={() => dispatch({ type: "REMOVE", id: todo.id })}>
+            Remove
+          </button>
+        </div>
+      ))}
     </div>
   );
-    
 }
 
 export default App;
