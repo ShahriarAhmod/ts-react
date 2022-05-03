c23dcbadd002a5138100e983778ad6805a49e7c3
Shahriar Ahmod <ashahriyar71@gmail.com>
2022-05-03 08:03:03 +0600

    3rd

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,36 @@
-import React from 'react';
+import React, { useReducer } from 'react';
 import logo from './logo.svg';
 import './App.css';
-import Lists from './components/Lists';
+// import Lists from './components/Lists';
 
+// const Box: React.FunctionComponent<{title: string}> = ({title}) => {
+//   return <div></div>;
+// };
+interface todo {
+  id: number;
+  text: string;
+}
+type ActionType = {tyoe: "ADD"; text: string} | {type: "REMOVE"; id: string};
 
 function App() {
-  return <div className="App">
-    <Lists />
+  function reducer(state:Todo[], action: ActionType) {
+    switch(action.type) {
+      case "ADD":
+        return [
+          ...state,
+          {
+            id: state.length,
+          }
+        ]
+  }
+}
+  const [todos, dispatch] = useReducer(reducer, []);
+  return (
+  <div className="App">
+    {/* <Box title="hi"></Box>
+    <Lists /> */}
     </div>
-    
+  );
     
 }
 
